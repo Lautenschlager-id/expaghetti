8747667ee71c51cd9dd4c0a353a915417b1f298a
Lautenschlager-id <tai.romani@hotmail.com>
2019-07-21 23:47:44 -0300

    Fixed performance issues

--- util.lua
+++ util.lua
@@ -1,6 +1,4 @@
 local strsub = string.sub
-local tblinsert = table.insert
-local tblremove = table.remove
 
 local createSet = function(array)
 	local set = { }
@@ -37,24 +35,16 @@ local insert = function(src, pos, tbl, ignore)
 	ignore = ignore or 0
 
 	local tblLen = #tbl
-	--[[local realLen = tblLen - ignore
+	local realLen = tblLen - ignore
 
-	for i = pos + ignore, #src do
+	for i = #src, pos + ignore , -1 do
 		src[realLen + i] = src[i]
 	end
 	for i = pos, pos + tblLen - 1 do
 		src[i] = tbl[i - pos + 1]
 	end
 
-	return realLen]] -- ↑ Needs more debugging to find the problems
-	for i = 1, ignore do
-		tblremove(src, pos)
-	end
-	for i = tblLen, 1, -1 do
-		tblinsert(src, pos, tbl[i])
-	end
-
-	return tblLen - ignore
+	return realLen
 end
 
 return {
