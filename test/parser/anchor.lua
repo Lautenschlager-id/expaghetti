1dc012612ad9e4ae92b1d740217b0984f6c690f6
Lautenschlager-id <tai.romani@hotmail.com>
2019-09-15 16:13:52 -0300

    Add anchor parsing tests

--- test/parser/anchor.lua
+++ test/parser/anchor.lua
@@ -1,5 +1,39 @@
 local test = require("../../test/util")
 
+test.assertion.object(
+	'^',
+	{
+		type = "anchor",
+		anchor = '^'
+	},
+	"The anchor '^' has failed."
+)
 
+test.assertion.object(
+	'$',
+	{
+		type = "anchor",
+		anchor = '$'
+	},
+	"The anchor '$' has failed."
+)
+
+test.assertion.object(
+	"%b",
+	{
+		type = "anchor",
+		anchor = 'b'
+	},
+	"The anchor '%b' has failed."
+)
+
+test.assertion.object(
+	"%B",
+	{
+		type = "anchor",
+		anchor = 'B'
+	},
+	"The anchor '%B' has failed."
+)
 
 return test.assertion.get()
