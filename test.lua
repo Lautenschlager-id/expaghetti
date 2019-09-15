1544292d7b7d8622cb56f2a66e2a6bd421f34267
Lautenschlager-id <tai.romani@hotmail.com>
2019-09-15 16:13:25 -0300

    Added test structures

---
+++ test.lua
@@ -0,0 +1,17 @@
+local strgmatch = string.gmatch
+local strformat = string.format
+local strsub = string.sub
+local popen = io.popen
+
+local folder = {
+	"parser"
+}
+
+local getFilesWithoutExtension = (strsub(package.config, 1, 1) == "\\" and "for %%a in (\"test/%s/*\") do @echo %%~na" or "ls 'test/%s/' -1 | sed -e 's/\\..*$//'") -- Windows (\) or Linux (/)
+
+for f = 1, #folder do
+	f = folder[f]
+	for file in strgmatch(popen(strformat(getFilesWithoutExtension, f)):read("*a"), "[^\n]+") do
+		print("[" .. file .. "] " .. (require("test/" .. f .. "/" .. file) or "All tests have passed."))
+	end
+end
--- test/util.lua
+++ test/util.lua
@@ -6,6 +6,10 @@ do
 	local strfind = string.find
 
 	tableToString = function(tbl, indent, numIndex, stop, _depth, _ref)
+		if type(tbl) ~= "table" then
+			return tostring(tbl)
+		end
+
 		if _depth and _depth > 1 and _ref == tbl then
 			return tostring(_ref)
 		end
@@ -61,7 +65,35 @@ do
 	end
 end
 
+local assertion = {
+	error = nil
+}
+
+assertion.get = function()
+	local out = assertion.error or false -- forces boolean
+	assertion.error = nil
+	return out
+end
+
+do
+	local parse = require("../parser")
+
+	assertion.object = function(src, comp, msg, checkTree)
+		if assertion.error then return end
+
+		src = parse(src).stack
+		if not checkTree then
+			src = src[1]
+		end
+
+		if not tableCompare(src, comp) then
+			assertion.error = msg .. "\n\tGot:\n\t\t" .. tableToString(src) .. "\n\tExpected:\n\t\t" .. tableToString(comp)
+		end
+	end
+end
+
 return {
 	tableToString = tableToString,
-	tableCompare = tableCompare
+	tableCompare = tableCompare,
+	assertion = assertion
 }
