d618600b82ffe0c38d2668c7865810864746e1f8
Lautenschlager-id <tai.romani@hotmail.com>
2019-11-13 23:38:12 -0300

    Add test workflow for pushes and pull requests

--- test.lua
+++ test.lua
@@ -11,9 +11,16 @@ local folder = {
 
 local getFilesWithoutExtension = (strsub(package.config, 1, 1) == "\\" and "for %%a in (\"test/%s/*\") do @echo %%~na" or "ls 'test/%s/' -1 | sed -e 's/\\..*$//'") -- Windows (\) or Linux (/)
 
+local hasFailed, testError = false
 for f = 1, #folder do
 	f = folder[f]
 	for file in strgmatch(popen(strformat(getFilesWithoutExtension, f)):read("*a"), "[^\n]+") do
-		print("[" .. f .. "/" .. file .. "] " .. (require("test/" .. f .. "/" .. file) or "All tests have passed."))
+		testError = require("test/" .. f .. "/" .. file)
+		if testError and not hasFailed then
+			hasFailed = true
+		end
+		print("[" .. f .. "/" .. file .. "] " .. (testError or "All tests have passed."))
 	end
 end
+
+return (hasFailed and error()) -- ".github/workflows/test.yml"'s purpose
