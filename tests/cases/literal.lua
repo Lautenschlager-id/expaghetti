4d13e113f656d8562df4c654465ca9cda7bfda4a
Lautenschlager <tai.romani@hotmail.com>
2022-08-24 00:20:53 -0300

    [T1] Create Basic Test Functions
    * create table comparison
    
    * create basic test running

---
+++ tests/cases/literal.lua
@@ -0,0 +1,20 @@
+return {
+	{
+		regex = "abc",
+		parsed = {
+			_index = 3,
+			{
+				type = "literal",
+				value = 'a'
+			},
+			{
+				type = "literal",
+				value = 'b'
+			},
+			{
+				type = "literal",
+				value = 'c'
+			}
+		}
+	}
+}
