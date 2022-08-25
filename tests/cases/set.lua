1eed17bc27be8cce9d28bb0fc19e24a48047f9aa
Lautenschlager <tai.romani@hotmail.com>
2022-08-24 23:28:43 -0300

    test set with character classes

--- tests/cases/set.lua
+++ tests/cases/set.lua
@@ -79,4 +79,158 @@ return {
 			}
 		}
 	},
+	{
+		regex = "[%w.%a^%U]",
+		parsed = {
+			_index = 1,
+			{
+				type = "set",
+				hasToNegateMatch = false,
+				rangeIndex = 0,
+				ranges = { },
+				classIndex = 3,
+				classes = {
+					{
+						type = "set",
+
+						hasToNegateMatch = false,
+
+						rangeIndex = 6,
+						ranges = {
+							'0',
+							'9',
+
+							'a',
+							'z',
+
+							'A',
+							'Z'
+						},
+
+						['_'] = true
+					},
+					{
+						type = "set",
+
+						hasToNegateMatch = false,
+
+						rangeIndex = 4,
+						ranges = {
+							'a',
+							'z',
+
+							'A',
+							'Z'
+						}
+					},
+					{
+						type = "set",
+
+						hasToNegateMatch = true,
+
+						rangeIndex = 2,
+						ranges = {
+							'A',
+							'Z'
+						}
+					}
+				},
+				['.'] = true,
+				['^'] = true
+			}
+		}
+	},
+	{
+		regex = "[%w.-~%aa^%U]",
+		parsed = {
+			_index = 1,
+			{
+				type = "set",
+				hasToNegateMatch = false,
+				rangeIndex = 2,
+				ranges = {
+					'.', '~'
+				},
+				classIndex = 3,
+				classes = {
+					{
+						type = "set",
+
+						hasToNegateMatch = false,
+
+						rangeIndex = 6,
+						ranges = {
+							'0',
+							'9',
+
+							'a',
+							'z',
+
+							'A',
+							'Z'
+						},
+
+						['_'] = true
+					},
+					{
+						type = "set",
+
+						hasToNegateMatch = false,
+
+						rangeIndex = 4,
+						ranges = {
+							'a',
+							'z',
+
+							'A',
+							'Z'
+						}
+					},
+					{
+						type = "set",
+
+						hasToNegateMatch = true,
+
+						rangeIndex = 2,
+						ranges = {
+							'A',
+							'Z'
+						}
+					}
+				},
+				['^'] = true,
+				['a'] = true
+			}
+		}
+	},
+	{
+		regex = "[^%UU-ZZ-]",
+		parsed = {
+			_index = 1,
+			{
+				type = "set",
+				hasToNegateMatch = true,
+				rangeIndex = 2,
+				ranges = {
+					'U', 'Z'
+				},
+				classIndex = 1,
+				classes = {
+					{
+						type = "set",
+
+						hasToNegateMatch = true,
+
+						rangeIndex = 2,
+						ranges = {
+							'A',
+							'Z'
+						}
+					}
+				},
+				['Z'] = true,
+				['-'] = true
+			}
+		}
+	},
 }
