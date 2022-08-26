8500bf550aebd327fc4922d7b53357bb20518060
Lautenschlager <tai.romani@hotmail.com>
2022-08-25 23:58:15 -0300

    test groups and nested groups

---
+++ tests/cases/group.lua
@@ -0,0 +1,96 @@
+return {
+	{
+		regex = "(a)",
+		parsed = {
+			_index = 1,
+			{
+				type = "group",
+				tree = {
+					_index = 1,
+					{
+						type = "literal",
+						value = 'a'
+					}
+				}
+			}
+		}
+	},
+	{
+		regex = "a(b)c",
+		parsed = {
+			_index = 3,
+			{
+				type = "literal",
+				value = 'a'
+			},
+			{
+				type = "group",
+				tree = {
+					_index = 1,
+					{
+						type = "literal",
+						value = 'b'
+					}
+				}
+			},
+			{
+				type = "literal",
+				value = 'c'
+			}
+		}
+	},
+	{
+		regex = "a(b((%((%.))%)))c",
+		parsed = {
+			_index = 3,
+			{
+				type = "literal",
+				value = 'a'
+			},
+			{
+				type = "group",
+				tree = {
+					_index = 2,
+					{
+						type = "literal",
+						value = 'b'
+					},
+					{
+						type = "group",
+						tree = {
+							_index = 2,
+							{
+								type = "group",
+								tree = {
+									_index = 2,
+									{
+										type = "literal",
+										value = '('
+									},
+									{
+										type = "group",
+										tree = {
+											_index = 1,
+											{
+												type = "literal",
+												value = '.'
+											}
+										}
+									}
+								}
+							},
+							{
+								type = "literal",
+								value = ')'
+							}
+						},
+					}
+				}
+			},
+			{
+				type = "literal",
+				value = 'c'
+			}
+		}
+	},
+}
