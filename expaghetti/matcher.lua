272a1878abc6d6bddb39dc3d421ec5e2fb7923c3
Lautenschlager <tai.romani@hotmail.com>
2022-08-27 13:23:56 -0300

    split string and parse expression

---
+++ expaghetti/matcher.lua
@@ -0,0 +1,26 @@
+----------------------------------------------------------------------------------------------------
+local splitStringByEachChar = require("./helpers/string").splitStringByEachChar
+----------------------------------------------------------------------------------------------------
+local parser = require("./parser")
+----------------------------------------------------------------------------------------------------
+local ENUM_FLAG_UNICODE = require("./enums/flags").UNICODE
+----------------------------------------------------------------------------------------------------
+local function matcher(expr, str, flags)
+	flags = flags or { }
+
+	local tree = parser(expr, flags)
+	local splitStr, strLength = splitStringByEachChar(str, not not flags[ENUM_FLAG_UNICODE])
+
+	return splitStr
+end
+
+----------------------------------------------------------------------------------------------------
+-- Debugging
+local p = require("./helpers/pretty-print")
+local see = function(t) print(p(t, true)) end
+
+see(matcher("abc", "not abacate"))
+
+----------------------------------------------------------------------------------------------------
+
+return matcher
