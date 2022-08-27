ddc29110228570a73f81bda9790dc2e3daafcbad
Lautenschlager <tai.romani@hotmail.com>
2022-08-27 13:40:54 -0300

    match literal at any position of the string

--- expaghetti/matcher.lua
+++ expaghetti/matcher.lua
@@ -3,15 +3,50 @@ local splitStringByEachChar = require("./helpers/string").splitStringByEachChar
 ----------------------------------------------------------------------------------------------------
 local parser = require("./parser")
 ----------------------------------------------------------------------------------------------------
+--local Anchor = require("./magic/anchor")
+--local Alternate = require("./magic/alternate")
+--local Any = require("./magic/any")
+--local Group = require("./magic/group")
+local Literal = require("./magic/literal")
+--local Quantifier = require("./magic/Quantifier")
+--local Set = require("./magic/set")
+----------------------------------------------------------------------------------------------------
 local ENUM_FLAG_UNICODE = require("./enums/flags").UNICODE
 ----------------------------------------------------------------------------------------------------
 local function matcher(expr, str, flags)
 	flags = flags or { }
 
 	local tree = parser(expr, flags)
+	local treeLength = tree._index
+
 	local splitStr, strLength = splitStringByEachChar(str, not not flags[ENUM_FLAG_UNICODE])
 
-	return splitStr
+	local treeIndex, stringIndex = 0, 0
+	local currentElement, currentCharacter
+	local hasMatched
+
+	while treeIndex < treeLength do
+		treeIndex = treeIndex + 1
+		currentElement = tree[treeIndex]
+
+		repeat
+			stringIndex = stringIndex + 1
+			currentCharacter = splitStr[stringIndex]
+
+			if not currentCharacter then
+				return
+			else
+				hasMatched = Literal.match(currentElement, currentCharacter)
+			end
+
+			if not hasMatched then
+				treeIndex = 0
+				break
+			end
+		until hasMatched
+	end
+
+	return true
 end
 
 ----------------------------------------------------------------------------------------------------
@@ -19,9 +54,7 @@ end
 local p = require("./helpers/pretty-print")
 local see = function(t) print(p(t, true)) end
 
-see(matcher("abc", "not abacate"))
-see(matcher("abc", "abacatãozão", { ['u'] = true }))
-see(matcher("abc", "abacatãozão", { ['u'] = false }))
+see(matcher("%cCo ", "abacateiro d\3o abc!"))
 
 ----------------------------------------------------------------------------------------------------
 
