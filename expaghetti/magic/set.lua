af3907703fcf35bce83d9ea3708656fe68f2c654
Tai <tai.romani@hotmail.com>
2026-07-23 02:44:52 -0300

    adjust set.lua

--- expaghetti/magic/set.lua
+++ expaghetti/magic/set.lua
@@ -1,26 +1,40 @@
 --[[
-    Parses set elements.
+    Parser and matcher for character sets (`[...]`).
+
+    Supports literal characters, ranges, nested character classes,
+    and negated sets.
 ]]
 
 --[[ Dependencies ]]--
-local magicEnum = require("enums.magic")
-local errorsEnum = require("enums.errors")
-local elementsEnum = require("enums.elements")
-local AST = require("ast")
+local SetNode = require("ast").Set
 
---[[ Enum Aliases ]]--
-local MAGIC_SET_OPEN = magicEnum.SET_OPEN
-local MAGIC_SET_CLOSE = magicEnum.SET_CLOSE
-local MAGIC_SET_NEGATE_PREFIX = magicEnum.SET_NEGATE_PREFIX
-local MAGIC_SET_RANGE_SEPARATOR = magicEnum.SET_RANGE_SEPARATOR
-local ELEMENT_SET = elementsEnum.SET
-local ERROR_UNCLOSED_SET = errorsEnum.unterminatedSet
-local ERROR_UNORDERED_SET_RANGE = errorsEnum.unorderedSetRange
+--[[ Enums ]]--
+local Elements = require("enums.elements")
+local Errors = require("enums.errors")
+local Magic = require("enums.magic")
+
+--[[ Aliases ]]--
+local ELEMENT_SET = Elements.SET
+
+local ERROR_UNTERMINATED_SET = Errors.unterminatedSet
+local ERROR_UNORDERED_SET_RANGE = Errors.unorderedSetRange
+
+local MAGIC_SET_CLOSE = Magic.SET_CLOSE
+local MAGIC_SET_OPEN = Magic.SET_OPEN
+
+local MAGIC_SET_NEGATE_PREFIX = Magic.SET_NEGATE_PREFIX
+local MAGIC_SET_RANGE_SEPARATOR = Magic.SET_RANGE_SEPARATOR
 
 --[[ Module ]]--
 local Set = {}
 
 --[[ Private Functions ]]--
+
+--- Finds the closing delimiter of a character set.
+---@param state ParserState The current parser state.
+---@param startIndex number The parser index immediately after the opening delimiter.
+---@return number|false endIndex The index of the closing delimiter, or false if none was found.
+---@return string|nil errorMessage The parser error message on failure.
 local findSetClosingIndex = function(state, startIndex)
 	local currentIndex = startIndex
 
@@ -32,11 +46,12 @@ local findSetClosingIndex = function(state, startIndex)
 	-- Skip the mandatory first character of the set.
 	local nextIndex = state:readElement(currentIndex, true)
 	if not nextIndex then
-		return false, ERROR_UNCLOSED_SET
+		return false, ERROR_UNTERMINATED_SET
 	end
 
+	local statePatternLength = state.patternLength
 	currentIndex = nextIndex
-	while currentIndex <= state.patternLength do
+	while currentIndex <= statePatternLength do
 		local nextIndex, element = state:readElement(currentIndex, true)
 
 		if element == MAGIC_SET_CLOSE then
@@ -46,26 +61,45 @@ local findSetClosingIndex = function(state, startIndex)
 		currentIndex = nextIndex
 	end
 
-	return false, ERROR_UNCLOSED_SET
+	return false, ERROR_UNTERMINATED_SET
 end
 
+--- Adds a character range to a set.
+---@param set SetNode The set being built.
+---@param startChar string|number The first character of the range.
+---@param endChar string|number The last character of the range.
 local addRange = function(set, startChar, endChar)
-	set.rangeIndex = set.rangeIndex + 1
-	set.ranges[set.rangeIndex] = startChar
+	local rangeIndex = set.rangeIndex + 1
+	local ranges = set.ranges
+
+	ranges[rangeIndex] = startChar
 	
-	set.rangeIndex = set.rangeIndex + 1
-	set.ranges[set.rangeIndex] = endChar
+	rangeIndex = rangeIndex + 1
+	ranges[rangeIndex] = endChar
+
+	set.rangeIndex = rangeIndex
 end
 
 --[[ Public API ]]--
+
+--- Returns whether a character starts a character set.
+---@param currentCharacter string The current pattern character.
+---@return boolean isSet Whether the character starts a character set.
 Set.isToken = function(currentCharacter)
 	return currentCharacter == MAGIC_SET_OPEN
 end
 
+--- Returns whether an AST element is a character set node.
+---@param currentElement table The AST element to test.
+---@return boolean isSet Whether the element is a character set node.
 Set.isElement = function(currentElement)
 	return currentElement.type == ELEMENT_SET
 end
 
+--- Parses a character set.
+---@param state ParserState The current parser state.
+---@param tree ASTTree The AST tree being built.
+---@return string|nil errorMessage The parser error message on failure.
 Set.parse = function(state, tree)
 	-- skip magic opening
 	state.index = state.index + 1
@@ -78,11 +112,13 @@ Set.parse = function(state, tree)
 	-- Set boundary [index, endIndex)
 	endIndex = endIndex - 1
 
-	local set = AST.Set()
+	local set = SetNode()
 	local isCaseInsensitive = state.flags.i
 
 	local watchingForRangeSeparator
 	local rangeInitChar
+
+	local setValues = set.values
 
 	local elementIndex = state.index
 	while elementIndex <= endIndex do
@@ -99,11 +135,12 @@ Set.parse = function(state, tree)
 		if originalElementIndex == state.index and element == MAGIC_SET_NEGATE_PREFIX then
 			set.hasToNegateMatch = true
 		elseif element.type == ELEMENT_SET then
-			set.classIndex = set.classIndex + 1
-			set.classes[set.classIndex] = element
+			local classIndex = set.classIndex + 1
+			set.classIndex = classIndex
+			set.classes[classIndex] = element
 		else
 			local currentCharacterValue = element.value or state:getExecutionValues(element, true)
-			local nextTokenValue, nextTokenIsRangeSep, skipCount
+			local skipCount, nextTokenValue, nextTokenIsRangeSep = 0
 
 			if endIndex >= elementIndex then
 				local peekIndex, nextElement = state:readElement(elementIndex, true)
@@ -128,31 +165,37 @@ Set.parse = function(state, tree)
 					addRange(set, rangeInitChar, nextTokenValue)
 
 					-- skip next element(s)
-					elementIndex = elementIndex + (skipCount or 0)
+					elementIndex = elementIndex + skipCount
 				else
-					set.values[rangeInitChar] = true
-					set.values[currentCharacterValue] = true
+					setValues[rangeInitChar] = true
+					setValues[currentCharacterValue] = true
 				end
 
 				rangeInitChar = nil
 			elseif nextTokenIsRangeSep then
 				rangeInitChar = currentCharacterValue
 			else
-				set.values[currentCharacterValue] = true
+				setValues[currentCharacterValue] = true
 			end
 		end
 	end
 	
 	state:compileSet(set)
 
-	tree._index = tree._index + 1
-	tree[tree._index] = set
+	local treeIndex = tree._index + 1
+	tree._index = treeIndex
+	tree[treeIndex] = set
 
 	-- skip magic closing (+ 1 to undo the boundary, then + 1)
 	state.index = endIndex + 2
 	return nil
 end
 
+--- Matches a character set against the target character.
+---@param currentElement SetNode The character set AST node.
+---@param state MatchState Unused matcher state.
+---@param currentCharacter string|number|nil The current target character.
+---@return boolean hasMatched Whether the character matched the set.
 Set.match = function(currentElement, _, currentCharacter)
 	local hasMatched = not not currentElement.values[currentCharacter]
 
@@ -169,9 +212,10 @@ Set.match = function(currentElement, _, currentCharacter)
 		end
 
 		if not hasMatched then
+			local SetMatch = Set.match
 			local classes = currentElement.classes
 			for classIndex = 1, currentElement.classIndex do
-				if Set.match(classes[classIndex], _, currentCharacter) then
+				if SetMatch(classes[classIndex], _, currentCharacter) then
 					hasMatched = true
 					break
 				end
