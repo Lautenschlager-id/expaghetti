e0051ca937bb7e3e67453e3bbe545cda25ff0e30
Tai <tai.romani@hotmail.com>
2026-07-24 22:25:07 -0300

    refactor: make config type validation pure and return values rather than mutating

--- expaghetti/config.lua
+++ expaghetti/config.lua
@@ -11,28 +11,23 @@ local globalConfig = {
 	maxBacktrackDepth = DEFAULT_MAX_BACKTRACK_DEPTH
 }
 
-local function enforceType(options, config, key, expectedType)
-	local val = options[key]
+local function validateType(val, expectedType, defaultVal, key)
 	if val ~= nil then
 		if type(val) ~= expectedType then
 			error("Config Error: " .. key .. " must be a " .. expectedType)
 		end
-		config[key] = val
+		return val
 	end
+	return defaultVal
 end
 
 local function buildConfig(options)
 	if type(options) ~= "table" then return globalConfig end
 	
-	local config = {
-		maxRecursionDepth = globalConfig.maxRecursionDepth,
-		maxBacktrackDepth = globalConfig.maxBacktrackDepth
+	return {
+		maxRecursionDepth = validateType(options.maxRecursionDepth, "number", globalConfig.maxRecursionDepth, "maxRecursionDepth"),
+		maxBacktrackDepth = validateType(options.maxBacktrackDepth, "number", globalConfig.maxBacktrackDepth, "maxBacktrackDepth")
 	}
-	
-	enforceType(options, config, "maxRecursionDepth", "number")
-	enforceType(options, config, "maxBacktrackDepth", "number")
-	
-	return config
 end
 
 return {
